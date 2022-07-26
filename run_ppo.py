1cd8c4f393122d649768b4305223e0f013844d91
dxg2913 <88538372+dxg2913@users.noreply.github.com>
2022-07-26 20:28:15 +0800

    Update run_ppo.py

--- run_ppo.py
+++ run_ppo.py
@@ -14,8 +14,13 @@ env = DummyVecEnv([lambda: ERP()])
 model = PPO("MlpPolicy", env, verbose=1)
 model.learn(total_timesteps=20000)
 
-obs = env.reset()
+
 for i in range(2000):
-    action, _states = model.predict(obs)
-    obs, rewards, done, info = env.step(action)
-    env.render()
+    obs = env.reset()
+    for i in range(200):
+        action, _states = model.predict(obs)
+        obs, rewards, done, info = env.step(action)
+        if done:
+            break
+
+        env.render()
